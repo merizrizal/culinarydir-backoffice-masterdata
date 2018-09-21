7fab685b570136a65611e60242ec7ed5833cd3e3
Mei Rizal <meriz.rizal@gmail.com>
2018-09-21 18:45:47 +0700

    Perbaikan code

--- views/province/_search.php
+++ views/province/_search.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model core\models\ProvinceSearch */
+/* @var $model core\models\search\ProvinceSearch */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
